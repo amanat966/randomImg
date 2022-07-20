22296da6ecae014ad08d2fe9fcdf27a3bc2e7bc4
amanat <text7434@gmail.com>
2022-07-21 02:24:41 +0600

    add ProgressBa loading

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.app
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,7 +8,11 @@ import android.view.inputmethod.EditorInfo
 import android.widget.RadioButton
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.app.databinding.ActivityMainBinding
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
@@ -23,16 +28,42 @@ class MainActivity : AppCompatActivity() {
         binding.btnGetImg.setOnClickListener {
             getSearchRandomImg()
         }
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            getSearchRandomImg()
+        }
     }
 
     private fun getSearchRandomImg(): Boolean {
         val checkedId = binding.radioGroup.checkedRadioButtonId
         val keyword = binding.radioGroup.findViewById<RadioButton>(checkedId).text.toString()
         val encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+        binding.progressBar.visibility = View.VISIBLE
         Glide.with(this)
             .load("https://source.unsplash.com/random/800*600?$?$encodedKeyword")
             .skipMemoryCache(true)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.visibility = View.GONE
+                    return false
+                }
+            })
             .into(binding.imgView)
         return false
     }
